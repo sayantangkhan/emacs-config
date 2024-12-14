3b0dba509ab93719500429e77790622c427da022
Sayantan Khan <sayantan@Sayantans-Mac-mini.local>
2024-12-14 10:09:01 -0500

    Using the macos config instead

---
+++ elpa/twilight-bright-theme-20130605.843/twilight-bright-theme-pkg.el
@@ -0,0 +1,12 @@
+(define-package "twilight-bright-theme" "20130605.843" "A Emacs 24 faces port of the TextMate theme" 'nil :commit "9859474333fee9f907474dbd8763f617e8bfd89c" :authors
+  '(("Jim Myhrberg" . "contact@jimeh.me"))
+  :maintainers
+  '(("Jim Myhrberg" . "contact@jimeh.me"))
+  :maintainer
+  '("Jim Myhrberg" . "contact@jimeh.me")
+  :keywords
+  '("themes")
+  :url "https://github.com/jimeh/twilight-bright-theme.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
--- transient/history.el
+++ transient/history.el
@@ -1,11 +1,1 @@
-((magit-branch nil)
- (magit-commit nil)
- (magit-dispatch nil)
- (magit-file-dispatch nil)
- (magit-gitignore nil)
- (magit-merge nil)
- (magit-pull nil)
- (magit-push nil)
- (magit-rebase
-  ("--autostash"))
- (magit-stash nil))
+nil
